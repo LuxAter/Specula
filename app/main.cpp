2ec361cd776f08cde2412913830abc4772d1691d
Arden Rasmussen <ardenisthebest@gmail.com>
2022-01-22 22:37:03 -0800

    feat: added initial cli app

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,19 @@
-#include <argparse/argparse.hpp>
+#include <chrono>
 #include <cstdlib>
 #include <iostream>
 
+#include <argparse/argparse.hpp>
+#include <fmt/chrono.h>
+#include <fmt/core.h>
+#include <specula/version.hpp>
+
 int main(int argc, char *argv[]) {
-  argparse::ArgumentParser app("specula", "0.1.0");
+  argparse::ArgumentParser app("specula", specula::version.to_string());
+
+  app.add_argument("-o", "--output")
+      .default_value(
+          fmt::format("{:%Y-%m-%d-%M-%H-%S}", std::chrono::system_clock::now()))
+      .help("output directory to save the image sequence to");
 
   try {
     app.parse_args(argc, argv);
